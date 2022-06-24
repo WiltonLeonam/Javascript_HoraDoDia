5a78e5738e1ae16cd24a31bb30a34b55b529b524
Wilton Leonam <leo.wiltons@gmail.com>
2022-06-24 11:20:51 -0300

    Primeiro Commit

---
+++ hora.js
@@ -0,0 +1,9 @@
+var horas = 3
+console.log(horas)
+if(horas >= 4 && horas < 12 ){
+  console.log('Bom dia !')
+} else if(horas >= 12 && horas <= 18 ){
+  console.log('Boa Tarde !')
+} else {
+  console.log('Boa Noite !')
+}
